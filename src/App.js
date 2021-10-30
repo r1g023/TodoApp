dfa598639695e700b975f9b112e0d97d0e10fd8d
rigo0523 <limon1285@gmail.com>
2021-10-29 23:54:09 -0500

    notes array added to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Notes from "../components/Notes";
 
-const App = () => (
-  <div class="App">
-    <h1>Add a Note</h1>
-    <Notes />
-  </div>
-);
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "First sticky note",
+      body: "One should never stop learning",
+    },
+  ]);
+  return (
+    <div class="App">
+      <h1>Add a Note</h1>
+      <Notes />
+    </div>
+  );
+};
 
 export default App;
