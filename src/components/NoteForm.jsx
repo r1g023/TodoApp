e506b1cd316e2fda4e94bec8a24a4df86e28726e
rigo0523 <limon1285@gmail.com>
2021-10-30 14:07:38 -0500

    nest notes within form on less styling

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -7,6 +7,7 @@ const NoteForm = (props) => {
     <form action="">
       <label htmlFor="title">Title</label>
       <input type="text" id="title" name="title" />
+
       <label htmlFor="note">Note</label>
       <textarea name="body" id="note"></textarea>
       {/* button to submit form/note */}
