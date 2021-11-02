c80a8a2dbc34a46d1c464ddcc82a1f7a8aeb268a
rigo0523 <limon1285@gmail.com>
2021-11-02 13:01:53 -0500

    readded note form and note list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
+import Notes from "./components/Notes";
+import NoteForm from "./components/NoteForm";
 
 const App = () => {
-  c;
+  const [notes, setNotes] = useState([
+    { id: 1, title: "first sticky note", body: "first note for my app" },
+  ]);
+  const addNewNote = (note) => {
+    const newNote = {
+      id: Date.now(),
+      title: note.title,
+      body: note.body,
+    };
+    setNotes([...notes, newNote]);
+  };
   return (
-    <div className="App">
-      <h1>Add a Note</h1>
+    <div className="app">
+      <h2>Add a Note</h2>
+      <NoteForm addNewNote={addNewNote} />
+      <Notes noteList={noteList} />
     </div>
   );
 };
