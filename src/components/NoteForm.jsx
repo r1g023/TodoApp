4fd5149375894013998800ce5a18b9792919dd27
rigo0523 <limon1285@gmail.com>
2021-11-02 16:55:58 -0500

    saved user input to local storage

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const NoteForm = (props) => {
   const [note, setNote] = useState({
     title: "",
     body: "",
   });
+
+  useEffect(() => {
+    //storing note
+    localStorage.setItem("note", JSON.stringify(note));
+  }, [note]);
 
   //handle changes
   const handleChanges = (e) => {
@@ -48,3 +53,7 @@ const NoteForm = (props) => {
 };
 
 export default NoteForm;
+
+// Local Storage – Data is stored in the browser’s memory, and it doesn’t expire even after the browser’s window is closed.
+
+// Session storage – Data exists as long as the browser’s window is not closed.
