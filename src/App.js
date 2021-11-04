5461f38004719226695ca7e00cb874ec1502e2c0
LimRi <57161327+rigo0523@users.noreply.github.com>
2021-11-03 23:45:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Notes from "./components/Notes";
 import NoteForm from "./components/NoteForm";
 
