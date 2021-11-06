355d18c0c91141fd72f3fa251610078e52a0b054
rigo0523 <limon1285@gmail.com>
2021-11-06 12:15:59 -0500

    implemented from scratch, data persists on local storage after adding note

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React, { useState, useEffect } from "react";
-import Notes from "./components/Notes";
-import NoteForm from "./components/NoteForm";
+import Note from "./components/Note";
+import noteList from "./note-list";
+import Form from "./components/Form";
 
 const App = () => {
-  return <div className="App">//</div>;
+  const [notes, setNotes] = useState(noteList);
+
+  useEffect(() => {
+    let data = localStorage.getItem("notes");
+    if (data) {
+      setNotes(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  let addNewNote = (note) => {
+    let newNote = {
+      id: Date.now(),
+      title: note.title,
+      body: note.body,
+    };
+    setNotes([...notes, newNote]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo App - Enter a Note!</h1>
+      <Form addNewNote={addNewNote} />
+      <Note notes={notes} />
+    </div>
+  );
 };
 
 export default App;
