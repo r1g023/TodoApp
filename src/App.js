4520aacef4f8bd165709f69f5af08284d00531f4
rigo0523 <limon1285@gmail.com>
2021-11-09 12:49:14 -0600

    redo from scratch-practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
-  return <div className="App">to do app</div>;
+  return (
+    <div className="App">
+      <h1>Add a Note</h1>
+    </div>
+  );
 };
 
 export default App;
