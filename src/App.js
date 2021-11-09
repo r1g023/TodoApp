722259b22b1189fa2ab21c8397e3250497a837ea
rigo0523 <limon1285@gmail.com>
2021-11-09 09:49:02 -0600

    readded todoApp with local storage for both form/array of notes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React, { useState, useEffect } from "react";
+import Note from "./components/Note";
+import NoteForm from "./components/NoteForm";
+import noteList from "./note-list";
 
 const App = () => {
-  return <div className="App">to do app</div>;
+  const [notes, setNotes] = useState(noteList);
+
+  useEffect(() => {
+    let data = localStorage.getItem("notes");
+    if (data) {
+      setNotes(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  const addNewNote = (notee) => {
+    console.log("notee--->", notee);
+    let newNote = {
+      id: Date.now(),
+      title: notee.title,
+      body: notee.body,
+    };
+    setNotes([...notes, newNote]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Add a Note</h1>
+      <NoteForm addNewNote={addNewNote} />
+      <Note notes={notes} />
+    </div>
+  );
 };
 
 export default App;
