5745d82821182fdc567b91ac805f839a6460aa6e
rigo0523 <limon1285@gmail.com>
2021-11-16 16:51:36 -0600

    reimplent app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 const App = () => {
   return (
     <div className="App">
-      <h1>Add a Note</h1>
+      <h1>Add a Note .</h1>
     </div>
   );
 };
