ed0ed19881ffb23bd7e48e44991c80c96c17a2ef
rigo0523 <limon1285@gmail.com>
2021-11-24 09:44:46 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const App = () => {
       ...notes,
       noteList: notes.noteList.map((item) => {
         if (item.id === itemID) {
+          console.log(item.completed);
           return {
             ...item,
             completed: !item.completed,
