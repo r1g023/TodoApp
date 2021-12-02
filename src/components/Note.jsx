b36220565c573a532bd5bc1c054bdde5535782b6
rigo0523 <limon1285@gmail.com>
2021-12-01 21:26:44 -0600

    removed LS from card

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,31 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const Note = ({ data, toggleComplete }) => {
-  const [toggle, setToggle] = useState({ slider: false });
-
-  //save toggle button on localstorage
-  useEffect(() => {
-    let data = localStorage.getItem("toggle");
-    if (data) {
-      setToggle(JSON.parse(data));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("toggle", JSON.stringify(toggle));
-  }, [toggle]);
-
-  //toggle slidr button on / off
-  const handleSliderButton = (e) => {
-    e.persist();
-    console.log(e.target.name, ": ", e.target.value, e.target.checked);
-    setToggle({
-      ...toggle,
-      [e.target.name]:
-        e.target.type === "checkbox" ? e.target.checked : e.target.value,
-    });
-  };
-
+function Note({ data, deleteNote, toggleCard }) {
   return (
     <div className="note">
       <code>Note ID# {data.id}</code>
@@ -35,21 +10,14 @@ const Note = ({ data, toggleComplete }) => {
       </div>
       {/* delete card button   */}
 
-      <label htmlFor="slider" className="switch">
-        <input
-          type="checkbox"
-          name="slider"
-          id="slider"
-          onChange={handleSliderButton}
-          checked={toggle.slider}
-          onClick={() => toggleComplete(data.id)}
-        />
+      <label htmlFor="terms" className="switch">
+        <input type="checkbox" name="terms" id="terms" />
         <span className="slider round"></span>
       </label>
 
       <i className="fa fa-trash-o"></i>
     </div>
   );
-};
+}
 
 export default Note;
